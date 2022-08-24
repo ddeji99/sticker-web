55a9a690cbe3bb99a95a2a47d2bee6c5a90901a6
ddeji99 <ddeji99521@gmail.com>
2022-08-25 02:11:48 +0900

    cors1

--- src/apollo.js
+++ src/apollo.js
@@ -31,10 +31,10 @@ const httpLink = new HttpLink({ uri: "https://sticker--backend.herokuapp.com/gra
 
 const uploadHttpLink = createUploadLink({
   uri:"https://sticker--backend.herokuapp.com/graphql", /*credentials: 'omit',*/credentials: 'include',  //'http://localhost:8000/graphql',"https://sticker--backend.herokuapp.com/graphql"'include'
-  headers: {
-    //'Access-Control-Allow-Origin': "https://stickerdata.s3.ap-northeast-2.amazonaws.com",
-      'Access-Control-Allow-Credentials': true,
-  }
+ // headers: {
+   //'Access-Control-Allow-Origin': "https://www.stickerdata.s3.ap-northeast-2.amazonaws.com",
+     // 'Access-Control-Allow-Credentials': true,
+ // }
 });
 
 
